6b49e0fb3fe3a4d6dfe163ac83c50ecde6381eff
Danila <da_buddha@bk.ru>
2022-04-10 00:24:58 +0300

    Добавил функционал автопоиска региона

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,26 +2,51 @@
     <HeaderMain/>
   <div class="container">
       <input v-on:input="search" placeholder="Начните вводить название места" type="text"> 
-      <ul v-for="item in 6" :key="item"><li>{{item}}</li></ul>
+      <ul  v-for="item in arrOfLocations" :key="item"><div class="wrapper">
+          <li>{{item.value}}</li><button  v-on:click="addToFavorite(item)">+</button></div><hr>
+      </ul>
+      <div v-if="this.query && !arrOfLocations.length">Результатов нет</div>
   </div>
 </template>
 
 <script>
 import HeaderMain from '@/components/HeaderMain.vue'
+import axios from 'axios'
+
 export default {
     components: {
         HeaderMain,
     },
     data(){
         return {
-            arrOfLocations:null
+            arrOfLocations:[],
+            token:'7b1d99ed6972e5a7c511659eafc3d2b66b4a8031',
+            query:'',
+            addedLocations:[]
+
         }
     },
     methods: {
-        search(e){
-            console.log(e.target.value)
+        async search(e){
+            this.query=e.target.value
+            const options = {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Authorization": "Token " + this.token
+                },
+               
+            }
+            await axios.get(`https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address?query=${this.query}&count=6&language=ru`,options).then(response => this.arrOfLocations=response.data.suggestions)
+            
+        },
+        addToFavorite(region) {
+           this.addedLocations=[{lat:region.data.geo_lat,lon:region.data.geo_lon,name:region.value,id:region.data.region_fias_id,isMy:false}]
+           console.log(this.addedLocations)
         }
-    }
+    },
 
 }
 </script>
@@ -47,5 +72,15 @@ input {
 }
 input:focus {
     border: rgb(199, 199, 199) solid 2px;
+}
+ul {
+    list-style: none;
+}
+.wrapper {
+    display: flex;
+    justify-content: space-between;
+}
+ul:last-child > hr {
+    display: none;
 }
 </style>
