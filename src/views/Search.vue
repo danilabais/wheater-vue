73ed5059b2d6851867dbf6bcf72ce0ce05643548
Danila <da_buddha@bk.ru>
2022-04-09 22:35:46 +0300

    Сохранение перед добавлением плагина по автокомплиту

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,12 +1,27 @@
 <template>
+    <HeaderMain/>
   <div class="container">
-      <input type="text">
+      <input v-on:input="search" placeholder="Начните вводить название места" type="text"> 
       <ul v-for="item in 6" :key="item"><li>{{item}}</li></ul>
   </div>
 </template>
 
 <script>
+import HeaderMain from '@/components/HeaderMain.vue'
 export default {
+    components: {
+        HeaderMain,
+    },
+    data(){
+        return {
+            arrOfLocations:null
+        }
+    },
+    methods: {
+        search(e){
+            console.log(e.target.value)
+        }
+    }
 
 }
 </script>
@@ -20,6 +35,17 @@ export default {
 }
 input {
     margin: 20px 0;
-    
+    background: rgb(231, 231, 231);
+    background-repeat: no-repeat;
+    width: 100%;
+    border-radius: 20px;
+    border: none;
+    padding: 10px 20px;
+    outline: none;
+    border: solid rgb(231, 231, 231)  2px;
+
+}
+input:focus {
+    border: rgb(199, 199, 199) solid 2px;
 }
 </style>
