44cbd70a6b24a9f8c0b207bbb48a55f5c08ebd51
Danila <da_buddha@bk.ru>
2022-04-19 00:17:28 +0500

    last commit

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -28,27 +28,27 @@
                     {{Math.round(item.main.temp)}} <span>&deg; C</span>
                 </div>
                 <div class="card__day">
-                    Sunday, 11 am
+                  {{item.weather[0].description}}
                 </div>
                 </div>
                 <div class="card__right">
-                <div class="card__chips card__chips-pink">Strong Winds</div>
-                <div class="card__chips card__chips-blue">{{item.weather[0].description}}</div>
+                <div class="card__chips card__chips-pink">Макс темп  {{item.main.temp_max}}</div>
+                <div class="card__chips card__chips-blue">Мин темп  {{item.main.temp_min}}</div>
                 
                 </div>
             </div>
-            <a href="#" class="card__more">
+            <!-- <a  href="#" class="card__more">
                 VIEW STATS
-            </a>
+            </a> -->
             <div class="additional">
                 <div class="additional__item">
-                <img src="@/assets/img/rain.svg" alt=""><div class="additional__text"><span>Precipitation</span><span>6%</span></div>
+                <img src="@/assets/img/rain.svg" alt=""><div class="additional__text"><span>Осадки</span><span>6%</span></div>
                 </div>
                 <div class="additional__item">
-                <img src="@/assets/img/humidity.svg" alt=""><div class="additional__text"><span>Humidity</span><span>{{item.main.humidity}}%</span></div>
+                <img src="@/assets/img/humidity.svg" alt=""><div class="additional__text"><span>Влажность</span><span>{{item.main.humidity}}%</span></div>
                 </div>
                 <div class="additional__item">
-                <img src="@/assets/img/wind.svg" alt=""><div class="additional__text"><span>Wind</span><span>19 km/h</span></div>
+                <img src="@/assets/img/wind.svg" alt=""><div class="additional__text"><span>Ветер</span><span>19 km/h</span></div>
                 </div>
             </div>
             </div>
@@ -86,7 +86,8 @@ export default {
   computed: {
     arr(){
       return this.$store.getters.weather
-    }
+    },
+   
   }
  
  
@@ -109,6 +110,9 @@ export default {
   color: #fff;
   text-decoration: underline;
   
+}
+.card__day {
+  text-transform: uppercase;
 }
 
 
